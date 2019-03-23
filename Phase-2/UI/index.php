f0ff396f1440606030c7717a1398df914d703a52
pmk21 <prithvikrishna49@gmail.com>
2019-03-23 21:12:24 +0530

    Added screenshots and updated code

--- Phase-2/UI/index.php
+++ Phase-2/UI/index.php
@@ -7,11 +7,20 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		.gap{
+			margin:100px;
+			border:100px;
+			padding:100px;
+			display:inline-block;
+		}
+			
+	</style>
 </head>
 <body>
-		<nav class="navbar navbar-expand-md navbar-light bg-light shadow">
-			<a href="index.php"><img src="pesu.png" class="img-fluid" style="width:15%;height: auto;"></a>
-			<a class="navbar-brand ml-auto text-secondary" href="index.php"><h3>TIMETABLE MANAGEMENT</h3></a>
+		<nav class="navbar navbar-default navbar-light bg-light shadow">
+			<a href="index.php"><img src="pes_logo.png" class="img-fluid"></a>
+			<a class="navbar-brand ml-auto text-secondary pull-left" href="index.php"><h3>TIMETABLE MANAGEMENT</h3></a>
         </nav>
 
 		<div class="container pt-5">
@@ -24,15 +33,16 @@
 					<label for="date"><strong>Date:</strong></label>
 					<input type="date" class="form-control" name="date" id="date" placeholder="Select the Date" required>
 				</div>
-			    <div class="form-group"> 
-					<button type="submit" class="btn btn-primary">Submit</button>
-			    </div>
+					<button type="submit" class="btn btn-primary" name="time_date">Particular Timetable</button>
+					<button type="submit" class="btn btn-primary" name="timetable">Full Timetable</button>
+					<button type="submit" class="btn btn-primary" name="free_slots">Free slots</button>
 			</form>
 		</div>
 		
-		<footer>
+		<footer class="footer fixed-bottom">
 			<p><center>Copyright &COPY; PES University. All Rights Reserved | Contact Us: +91 90000 00000</center></p>
 		</footer>
+		
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
--- Phase-2/UI/request.php
+++ Phase-2/UI/request.php
@@ -10,84 +10,121 @@
 </head>
 
 <body>
-		<nav class="navbar navbar-expand-md navbar-light bg-light shadow">
-			<a href="index.php"><img src="pesu.png" class="img-fluid" style="width:10%;height: auto;"></a>
+		<nav class="navbar navbar-default navbar-light bg-light shadow">
+			<a href="index.php"><img src="pes_logo.png" class="img-fluid"></a>
 			<a class="navbar-brand ml-auto text-secondary" href="index.php"><h3>TIMETABLE MANAGEMENT</h3></a>
         </nav>
 	<div class="container">
 		
 			<?php
 				
-				echo '<h1 class="pt-5">';
-
+				//echo '<h1 class="pt-5">';
+				
 				if(isset($_POST["initials"]))
 				{
-					echo $initials = strtoupper($_POST["initials"]);
+					$initials = strtoupper($_POST["initials"]);
+					
+					if (strpos($initials, "DR") !== false)
+					{
+						$str_arr = explode('.', $initials);
+						$str_arr[0] = "Dr";
+						$initials = implode(".", $str_arr);
+					}
 				}
 
 				if(isset($_POST["date"]))
 				{
 					$date = strtotime($_POST["date"]);
 					$day = date("l", $date);
-					echo ' '.$day;
 				}
-
-				echo '</h1><br>';
-
+				
+				//echo '</h1><br>';
 				//Need to enter database name, username and password
 				
-				$db_connection = pg_connect("host='localhost' port='5432' dbname='timetable_management' user='root' password='root	'") or die("unable to connect to database");
-				$query = "SELECT sem as SEM, section as SECTION, room_no as ROOM_NO, pno as PERIOD, start_time as START_TIME, end_time as END_TIME FROM assign_to WHERE initials = '$initials' AND assign_to.day = '$day'";
-				$result = pg_query($query);
+				$db_connection = pg_connect("host='localhost' port='5432' dbname='timetable_management' user='root' password='root'") or die("unable to connect to database");				
 				
-				if (pg_num_rows($result) > 0)
-				{
-					echo '<table class="table table-hover table-responsive table-striped">
-							<thead>
-								<tr>';
-
-					$i = 0;
-					while ($i < pg_num_fields($result))
+				function print_timetable($result)
+				{	
+					if (pg_num_rows($result) > 0)
 					{
-						$fieldName = pg_field_name($result, $i);
-						echo '<th style="text-align:center">' . strtoupper($fieldName) . '</th>';
-						$i = $i + 1;
-					}
-					echo '</tr></thead><tbody>';
-
-					$i = 0;
-					while ($row = pg_fetch_row($result)) 
-					{
-						echo '<tr>';
-						$count = count($row);
-						$y = 0;
-						while ($y < $count)
+						echo '<table class="table table-hover table-responsive table-striped">
+								<thead>
+									<tr>';
+						
+						$i = 0;
+						
+						while ($i < pg_num_fields($result))
 						{
-							$c_row = current($row);
-							echo '<td style="text-align:center">' . $c_row . '</td>';
-							next($row);
-							$y = $y + 1;
+							$fieldName = pg_field_name($result, $i);
+							echo '<th style="text-align:center">' . strtoupper($fieldName) . '</th>';
+							$i = $i + 1;
 						}
-
-						echo '</tr>';
-						$i = $i + 1;
+						echo '</tr></thead><tbody>';
+						
+						$i = 0;
+						
+						while ($row = pg_fetch_row($result)) 
+						{
+							echo '<tr>';
+							$count = count($row);
+							$y = 0;
+							while ($y < $count)
+							{
+								$c_row = current($row);
+								echo '<td style="text-align:center">' . $c_row . '</td>';
+								next($row);
+								$y = $y + 1;
+							}
+							echo '</tr>';
+							$i = $i + 1;
+						}
+						
+						pg_free_result($result);
+						
+						echo '</tbody></table>';
 					}
-
-					pg_free_result($result);
-
-					echo '</tbody></table>';
+					else
+					{
+						echo "<h2 class='jumbotron text-center'>No classes</h2>";
+					}
 				}
-				else
+
+				if (isset($_POST["timetable"]))
 				{
-					echo "<h2 class='jumbotron text-center'>No classes</h2>";
+					echo "<h1 class='pt-5'>".$initials."'s Timetable for the Week</h1>";
+					$query = "SELECT sem as SEM, section as SECTION, room_no as ROOM_NO, pno as PERIOD,day as DAY, start_time as START_TIME, end_time as END_TIME FROM assign_to WHERE initials = '$initials'";
+					$result = pg_query($query);
+					print_timetable($result);
 				}
 
+				elseif (isset($_POST["free_slots"]))
+				
+				{
+					echo "<h1 class='pt-5'>".$initials."'s Free Slots</h1>";
+					$query = "SELECT * from timetable where (pno,day) NOT IN (SELECT pno, day FROM assign_to WHERE initials='$initials');";
+					$result = pg_query($query);
+					print_timetable($result);
+					
+				}
+
+				elseif (isset($_POST["time_date"])) 
+				{
+					echo "<h1 class='pt-5'>".$initials."'s Timetable</h1>";
+					$query = "SELECT sem as SEM, section as SECTION, room_no as ROOM_NO, pno as PERIOD, start_time as START_TIME, end_time as END_TIME FROM assign_to WHERE initials = '$initials' AND assign_to.day = '$day'";
+					$result = pg_query($query);
+					print_timetable($result);
+				}
+					
 				echo '<br/>';
 
 				pg_close($db_connection);
+			
 			?>
 		
 		<a href="index.php"><button class="btn btn-primary">Go Back</button></a>
+		<footer class="footer">
+			<p><center>Copyright &COPY; PES University. All Rights Reserved | Contact Us: +91 90000 00000</center></p>
+		</footer>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	
 	</div>
