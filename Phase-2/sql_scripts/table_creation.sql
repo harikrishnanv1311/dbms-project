c9994f94d7a3fdc08d960778aa0d7da8f13d5683
harikrishnanv1311 <48327826+harikrishnanv1311@users.noreply.github.com>
2019-04-20 02:31:12 +0530

    Update table_creation.sql

--- Phase-2/sql_scripts/table_creation.sql
+++ Phase-2/sql_scripts/table_creation.sql
@@ -21,55 +21,56 @@ CREATE TABLE timetable
 CREATE TABLE sem_class
 (	Sem INT NOT NULL,
   Section CHAR NOT NULL,
-  Room_No VARCHAR(6) NOT NULL,
+  Room_No VARCHAR(12) NOT NULL,
   Year INT NOT NULL,
 	PRIMARY KEY (Sem,Section,Room_No));
 
 CREATE TABLE course
-(	C_Code VARCHAR(6) NOT NULL,
+(	C_Code VARCHAR(12) NOT NULL,
   C_Name VARCHAR(15) NOT NULL,
   Credits INT NOT NULL,
 	PRIMARY KEY (C_Code),
   UNIQUE (C_Name));
 
 CREATE TABLE lecturer
-(	Initials VARCHAR(3) NOT NULL,
-	Name VARCHAR(15) NOT NULL,
+(	Initials VARCHAR(10) NOT NULL,
+	Name VARCHAR(20) NOT NULL,
   Dept VARCHAR(15) NOT NULL,
-  C_Code VARCHAR(6) NOT NULL,
-	PRIMARY KEY (Initials),
+  C_Code VARCHAR(12) NOT NULL,
+	PRIMARY KEY (Initials,C_Code),
 	FOREIGN KEY (C_Code) REFERENCES course(C_Code));
 
 CREATE TABLE assign_to
-(	Initials VARCHAR(3) NOT NULL ,
+(	Initials VARCHAR(10) NOT NULL ,
+	C_Code VARCHAR(12) NOT NULL,
 	Sem INT NOT NULL,
 	Section CHAR NOT NULL,
-	Room_No VARCHAR(6) NOT NULL,
+	Room_No VARCHAR(12) NOT NULL,
   Day VARCHAR(15)  NOT NULL,
   PNo INT NOT NULL,
   Start_time TIME NOT NULL,
   End_time TIME NOT NULL,
-	PRIMARY KEY (Initials,Sem,Section,Room_No,Day,PNo,Start_time,End_time),
-	FOREIGN KEY (Initials) REFERENCES lecturer(Initials),
+	PRIMARY KEY (Initials,C_Code,Sem,Section,Room_No,Day,PNo,Start_time,End_time),
+	FOREIGN KEY (Initials,C_Code) REFERENCES lecturer(Initials,C_Code),
   FOREIGN KEY (Sem,Section,Room_No) REFERENCES sem_class(Sem,Section,Room_No),
   FOREIGN KEY (Day,PNo,Start_time,End_time) REFERENCES timetable(Day,PNo,Start_time,End_time));
 
 CREATE TABLE offers
-(	Initials VARCHAR(3) NOT NULL,
-	C_Code VARCHAR(6) NOT NULL,
+(	Initials VARCHAR(10) NOT NULL,
+	C_Code VARCHAR(12) NOT NULL,
   Sem INT NOT NULL,
 	Section CHAR NOT NULL,
-	Room_No VARCHAR(6) NOT NULL,
+	Room_No VARCHAR(12) NOT NULL,
 	PRIMARY KEY (Initials,C_Code,Sem,Section,Room_No),
-	FOREIGN KEY (Initials) REFERENCES lecturer(Initials),
+	FOREIGN KEY (Initials,C_Code) REFERENCES lecturer(Initials,C_Code),
   FOREIGN KEY (C_Code) REFERENCES course(C_Code),
   FOREIGN KEY (Sem,Section,Room_No) REFERENCES sem_class(Sem,Section,Room_No));
 
 CREATE TABLE offered_during
-(	C_Code VARCHAR(6) NOT NULL,
+(	C_Code VARCHAR(12) NOT NULL,
   Sem INT NOT NULL,
   Section CHAR NOT NULL,
-  Room_No VARCHAR(6) NOT NULL,
+  Room_No VARCHAR(12) NOT NULL,
 	PRIMARY KEY (C_Code,Sem,Section,Room_No),
   FOREIGN KEY (C_Code) REFERENCES course(C_Code),
   FOREIGN KEY (Sem,Section,Room_No) REFERENCES sem_class(Sem,Section,Room_No));
